979f560f379423b9f6e2abdcafdd50a56ec66762
Rafael César Rocha Monteiro <108206396+RafaRock1@users.noreply.github.com>
2022-11-04 09:14:19 -0300

    Create aula07.js

---
+++ aulas/aula07.js
@@ -0,0 +1,24 @@
+class carros {
+    marca;
+    cor;
+    gastoMedioPorKm;
+
+    constructor(marca, cor, kmPorL) {
+        this.marca = marca;
+        this.cor = cor;
+        this.gastoMedioPorKm = gastoMedioPorKm;
+    }
+
+}
+
+function calculoViajem(carro1) {
+    const preçoG = 5.10, gasdoL = gastoMedioPorKm, distanciaKm = 100;
+
+
+
+
+}
+
+const uno = new carros('Fiat', 'Prata', 1/12);
+
+console.log(uno);
